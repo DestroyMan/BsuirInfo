981b50eede8147c5b48b5394e2aa5bdb135d428d
Антон Романчук <Destroy@MacBook-Pro-Anton.local>
2015-09-06 23:02:44 +0300

    Initial Commit

---
+++ BsuirRss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BsuirRss
+//
+//  Created by Антон Романчук on 06.09.15.
+//  Copyright (c) 2015 Антон Романчук. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BsuirRss/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BsuirRss
+//
+//  Created by Антон Романчук on 06.09.15.
+//  Copyright (c) 2015 Антон Романчук. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
